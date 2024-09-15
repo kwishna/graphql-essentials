e07b1de159fc7316bb49dd9611f64066e305ad06
kwishna <kwishna@gmail.com>
2024-09-16 01:25:25 +0530

    buildSchema

--- data/schema.js
+++ data/schema.js
@@ -1,8 +1,40 @@
 import { buildSchema } from "graphql";
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+
+// v1
+// const schema = buildSchema(`  
+//     type Query {
+//         hello: String
+//     }
+// `)
+
+// v2
 const schema = buildSchema(`
+    type Product {
+        id: ID
+        name: String
+        description: String
+        price: Float
+        soldout: Boolean
+    }
+
     type Query {
-        hello: String
+        product: Product
     }
 `)
+
+// Construct a schema, using GraphQL schema language
+export const dieSchema = buildSchema(/* GraphQL */`
+    type RandomDie {
+      numSides: Int!
+      rollOnce: Int!
+      roll(numRolls: Int!): [Int]
+    }
+   
+    type Query {
+      getDie(numSides: Int): RandomDie
+    }
+  `)
+
 
 export default schema;
